f163170dc5272e2338ffe62823a36d0683077ce7
lee donghwi 이동휘 <78001478+leedonghwie@users.noreply.github.com>
2021-09-17 00:20:30 +0900

    pparn2.0 modified from HTML to JS

---
+++ views/pparnGrid.js
@@ -0,0 +1,31 @@
+const pparnGrid = document.getElementById("pparnGrid");
+
+const pparnArray = [
+    {
+        title: "8.24",
+        link: "../text/pparn/210824.html",
+    },
+    {
+        title: "8.30",
+        link: "../text/pparn/210830.html",
+    },
+    {
+        title: "8.31",
+        link: "../text/pparn/210831.html",
+    },
+];
+
+function arrayPush(){
+    i=0;
+    while(i<pparnArray.length){
+        console.log(pparnArray[i].title);
+        const div=document.createElement("div");
+        const createA = document.createElement("a");
+            createA.href=`${pparnArray[i].link}`;
+            createA.innerText=`${pparnArray[i].title}`;
+        div.appendChild(createA);
+        pparnGrid.prepend(div);
+        i=i+1;
+    };
+};
+arrayPush();
