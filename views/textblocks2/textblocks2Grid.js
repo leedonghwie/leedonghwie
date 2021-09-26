18ebf275ab09d0606a1edfe83c7d1d75012dfa27
donghwi <leedonghwie@gmail.com>
2021-09-26 22:34:39 +0900

    louis CK

---
+++ views/textblocks2/textblocks2Grid.js
@@ -0,0 +1,55 @@
+const textblocksGrid=document.getElementById("textblocksGrid");
+
+const textblocksArray = [
+    {
+        title: "‘층간소음’ 없는 방",
+        link: "https://donghwilee.com/9275",
+    },
+    {
+        title: "편견과 기질",
+        link: "https://donghwilee.com/9519",
+    },
+    {
+        title: "모두안녕...zzz",
+        link: "https://donghwilee.com/10417",
+    },
+    {
+        title: "도둑 맞은 ‘D’에 관한 세미나",
+        link: "https://donghwilee.com/10737",
+    },
+    {
+        title: "월터 화이트와 헤어지기",
+        link: "https://donghwilee.com/12183",
+    },
+    {
+        title: "[체르노빌]: 한 가지 관찰과 해석",
+        link: "https://donghwilee.com/13091",
+    },
+    {
+        title: "Desire as D",
+        link: "https://donghwilee.com/13945",
+    },
+    {
+        title: "대상 없음: 현시원의 열한 페이지",
+        link: "https://donghwilee.com/14289",
+    },
+    {
+        title: "루이ck, <sincerely>",
+        link: "../../text/textblocks2/louis_ck.html",
+    },
+];
+
+function arrayPush(){
+    i=0;
+    while(i<textblocksArray.length){
+        console.log(textblocksArray[i].title);
+        const div=document.createElement("div");
+        const createA = document.createElement("a");
+            createA.href=`${textblocksArray[i].link}`;
+            createA.innerText=`${textblocksArray[i].title}`;
+        div.appendChild(createA);
+        textblocksGrid.prepend(div);
+        i=i+1;
+    };
+};
+arrayPush();
