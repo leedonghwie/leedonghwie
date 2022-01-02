94785642ada37b8c5933d5f685497b81bf50b96c
donghwi <leedonghwie@gmail.com>
2022-01-02 11:39:24 +0900

    character count added

--- js/word_count.js
+++ js/word_count.js
@@ -3,7 +3,8 @@ const para = document.getElementsByTagName("p");
 const word_count = () => {
     for(let i=0; i < para.length; i++){
         const word = para[i].textContent.split(" ").length;
-        para[i].append(" ["+word+" words]");
+        const char = para[i].textContent.length;
+        para[i].append(" ["+word+" words, "+char+" chrs]");
     }
 }
 
