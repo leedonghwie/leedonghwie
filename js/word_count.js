587e0746c678a31a7fda85393e5230dfeaf38e20
donghwi <leedonghwie@gmail.com>
2022-06-10 16:02:40 +0900

    220610

--- js/word_count.js
+++ js/word_count.js
@@ -1,5 +1,6 @@
 const para = document.getElementsByTagName("p");
 
+/*
 const word_count = () => {
     for(let i=0; i < para.length; i++){
         const word = para[i].textContent.split(" ").length;
@@ -9,3 +10,22 @@ const word_count = () => {
 }
 
 word_count();
+*/
+
+
+let words_count = 0;
+let chars_count = 0;
+const total_word = () => {
+    for(let i=0; i < para.length; i++){
+        const word = para[i].textContent.split(" ").length;
+        const char = para[i].textContent.length;
+        words_count+=word;
+        chars_count+=char;         
+    }
+    const newBr=document.createElement("br");
+    para[para.length-1].append(newBr);    
+    para[para.length-1].append(" ["+words_count+" words, "+chars_count+" chrs]");
+}
+
+total_word();
+
