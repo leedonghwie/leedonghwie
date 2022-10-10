eb96d82f397f68b9c0bd3fb63ec4583d2c1da9a7
donghwi <leedonghwie@gmail.com>
2022-10-10 16:51:02 +0900

    talk with haboksun

--- views/textblocks1/textblocks1Grid.js
+++ views/textblocks1/textblocks1Grid.js
@@ -3,11 +3,11 @@ const textblocksGrid=document.getElementById("textblocksGrid");
 const textblocksArray = [
     {
         title: "작품의 죽음(html)",
-        link: "../../text/textblocks1/death_of_artwork.html",
+        link: "../../text/death_of_artwork.html",
     },
     {
         title: "예술과 개념(책에 수록됨)",
-        link: "../../text/textblocks1/art_and_concept.html",
+        link: "../../text/art_and_concept.html",
     },
     {
         title: "[번역] 예술은 게임이다: 왜 중요한 건 (예술과의) 고투인가 (html)",
@@ -21,6 +21,10 @@ const textblocksArray = [
         title: "예술은 사적이다: 한 가지 댓글에 답변하며(책에 수록됨)",
         link: "https://leetunnel.postype.com/post/11193649",
     },
+    {
+        title: "사진작가 하태우와의 대화: 하태우가 『시급하지만 인기는 없는 문제: 예술·언어·이론』 속 이동휘의 글에 대해 보충 설명을 요구함",
+        link: "../../text/Talk_with_Haboksun.html",
+    }
 ];
 
 function arrayPush(){
