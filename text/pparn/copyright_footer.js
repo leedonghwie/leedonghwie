d176171c00fd6c565f00bde076f2c8284dd76aff
donghwi <leedonghwie@gmail.com>
2023-04-07 23:37:37 +0900

    asdf

--- text/pparn/copyright_footer.js
+++
@@ -1,9 +0,0 @@
-function copyright_footer(){
-    const footer = document.querySelector("footer");
-    const copyright_year = new Date().getFullYear();
-    footer.innerHTML=`&copy; ${copyright_year} donghwi LEE`;
-    footer.style.fontSize="x-small";
-    footer.style.textAlign="center";
-    footer.style.marginTop="100px";
-};
-copyright_footer();
--- text/pparn/like_button.js
+++
@@ -1,9 +0,0 @@
-const like_Btn_div = document.getElementById("like_Btn");
-const like_Btn_button = like_Btn_div.querySelector(".button");
-function handleLikeClick(){
-    const like_Btn_i = document.getElementById("like_Btn_i");
-    like_Btn_i.className="fas fa-heart";
-    localStorage.setItem=""
-};
-
-like_Btn_button.addEventListener("click", handleLikeClick);
